f3884abe562260ef7c3f8b5ef626b00827b276ae
20350597 <isc20350597@gmail.com>
2024-02-15 11:53:01 -0600

    Create agenda and buttons

---
+++ botones/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+
+function App() {
+
+// hooks
+
+const [contador, setContador] = useState(0)
+  const handleButtonIncrementar = () => {
+    setContador(contador + 1)
+  }
+    return (
+ <div>
+  <hr />
+  <h1>Contador</h1>
+  <h2>{contador}</h2>
+  <button 
+  onClick={handleButtonIncrementar}
+  >
+    Incrementar
+    </button>
+
+   <button onClick={() => setContador(contador - 1)}>Decrementar</button>
+
+   <button onClick={() => setContador(contador * 0)}>Resetear</button>
+
+ </div>
+  )
+}
+
+export default App
